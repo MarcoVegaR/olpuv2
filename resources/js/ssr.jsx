8ba2c0564f7ae8065916db826252dfdc1254935e
MarcoVegaR <marcovega11@gmail.com>
2026-02-12 09:27:37 -0400

    fix: add Ziggy route() setup to SSR entry point
    route() was not defined during server-side rendering, causing 500 errors
    on any page using route(). Import Ziggy from vendor and configure
    globalThis.route in the SSR setup function.

--- resources/js/ssr.jsx
+++ resources/js/ssr.jsx
@@ -4,6 +4,7 @@ createInertiaApp
 } from '@inertiajs/react';
 import createServer from '@inertiajs/react/server';
 import ReactDOMServer from 'react-dom/server';
+import { route as routeFn } from '../../vendor/tightenco/ziggy/dist/index.esm.js';
 
 createServer((page) =>
     createInertiaApp({
@@ -15,7 +16,17 @@ createServer((page) =>
             });
             return pages[`./pages/${name}.tsx`];
         },
-        // prettier-ignore
-        setup: ({ App, props }) => <App {...props} />,
+        setup: ({ App, props }) => {
+            // Make Ziggy's route() available globally during SSR
+            const ziggy = props.initialPage.props.ziggy;
+            globalThis.route = (name, params, absolute, config) =>
+                routeFn(name, params, absolute, {
+                    ...ziggy,
+                    location: new URL(ziggy.url),
+                    ...config,
+                });
+
+            return <App {...props} />;
+        },
     }),
 );
